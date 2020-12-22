05d9ce75e9678d1f36edf9dcd2b98baf0c4ace7b
silentmx <silentmx@icloud.com>
2020-12-22 18:08:40 +0800

    优化翻译管道

--- projects/silentmx/ngx-star/locale/ngx-i18n.pipe.ts
+++ projects/silentmx/ngx-star/locale/ngx-i18n.pipe.ts
@@ -11,11 +11,11 @@ export class NgxI18nPipe implements PipeTransform {
     NgxI18nPipe.dataSource = new Map(Object.entries(data));
   }
 
-  transform(key: string, args: string[]): string {
+  transform(key: string, args: string[] = []): string {
     let value = NgxI18nPipe.dataSource.get(key);
     if (value) {
       let argSet = new Set<String>(value.match(/\{[0-9]*\}/g));
-      if (argSet.size > 0) {
+      if (argSet.size > 0 && args.length > 0) {
         let argMap = new Map(Array.from(argSet).map((item, index) => {
           return [
             item,
@@ -29,7 +29,7 @@ export class NgxI18nPipe implements PipeTransform {
       return value;
     } else {
       let argSet = new Set<String>(key.match(/\{[0-9]*\}/g));
-      if (argSet.size > 0) {
+      if (argSet.size > 0 && args.length > 0) {
         let argMap = new Map(Array.from(argSet).map((item, index) => {
           return [
             item,
