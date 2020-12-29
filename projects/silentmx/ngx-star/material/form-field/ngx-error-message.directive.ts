e7f533f23223015e7220f7b6ad1b7cfbbf1c359e
silentmx <silentmx@icloud.com>
2020-12-29 14:10:11 +0800

    修正ngxErrorMessage指令异常

--- projects/silentmx/ngx-star/material/form-field/ngx-error-message.directive.ts
+++ projects/silentmx/ngx-star/material/form-field/ngx-error-message.directive.ts
@@ -1,16 +1,14 @@
 import { AfterViewInit, Component, Injector } from '@angular/core';
-import { NgControl } from '@angular/forms';
 import { MatFormField } from '@angular/material/form-field';
 
 @Component({
-  selector: '[ngxErrorMessage]',
+  selector: 'mat-error[ngxErrorMessage]',
   template: '{{ errorMsg | ngxI18n: [labelStr, otherParams] }}'
 })
 export class NgxErrorMessageDirective implements AfterViewInit {
   errorMsg: string = "";
   labelStr: string = "";
   otherParams: string[] = [];
-  private control: NgControl;
 
   constructor(
     private injector: Injector,
@@ -20,62 +18,51 @@ export class NgxErrorMessageDirective implements AfterViewInit {
 
   ngAfterViewInit() {
     let formField = this.injector.get(MatFormField);
-    this.control = formField._control.ngControl;
+    let ngControl = formField._control.ngControl;
     if (formField._hasLabel()) {
       Promise.resolve(null).then(() => {
         this.labelStr = formField["_label"].nativeElement.firstElementChild.innerText;
       });
     }
-
-    Promise.resolve(null).then(() => {
-      this.updateError(this.control.status);
+    
+    ngControl.statusChanges.subscribe((state) => {
+      if (state === "INVALID") {
+        let errors = ngControl.errors;
+        this.otherParams = [];
+  
+        // just grab one error
+        const firstError = Object.keys(errors)[0];
+  
+        if (firstError === "required") {
+          this.errorMsg = "MissingRequiredField";
+          return;
+        }
+  
+        if (firstError === "minlength") {
+          this.errorMsg = "The field {0} must be a string or array type with a minimum length of '{1}'."
+          this.otherParams = [`${errors.minlength.requiredLength}`];
+          return;
+        }
+  
+        if (firstError === "maxlength") {
+          this.errorMsg = "The field {0} must be a string with a maximum length of {1}.";
+          this.otherParams = [`${errors.maxlength.requiredLength}`];
+          return;
+        }
+  
+        if (firstError === "email") {
+          this.errorMsg = "EmailFormatErrorMessage";
+          return;
+        }
+  
+        if (firstError === "pattern") {
+          this.errorMsg = "The field {0} must match the regular expression '{1}'.";
+          return;
+        }
+  
+        this.errorMsg = `${errors[firstError]}`;
+      }
     });
-
-    this.control.statusChanges.subscribe(this.updateError);
-  }
-
-  private updateError(state: "INVALID" | "VALID" | string) {
-    if (state === "INVALID") {
-      let errors = this.control.errors;
-      this.otherParams = [];
-
-      // just grab one error
-      const firstError = Object.keys(errors)[0];
-
-      if (firstError === "required") {
-        this.errorMsg = "MissingRequiredField";
-        return;
-      }
-
-      if (firstError === "minlength") {
-        this.errorMsg = "The field {0} must be a string or array type with a minimum length of '{1}'."
-        this.otherParams = [`${errors.minlength.requiredLength}`];
-        return;
-      }
-
-      if (firstError === "maxlength") {
-        this.errorMsg = "The field {0} must be a string with a maximum length of {1}.";
-        this.otherParams = [`${errors.maxlength.requiredLength}`];
-        return;
-      }
-
-      if (firstError === "email") {
-        this.errorMsg = "EmailFormatErrorMessage";
-        return;
-      }
-
-      if (firstError === "pattern") {
-        this.errorMsg = "The field {0} must match the regular expression '{1}'.";
-        return;
-      }
-
-      this.errorMsg = `${errors[firstError]}`;
-    }
-
-    if (state === "VALID") {
-      this.errorMsg = "";
-      this.otherParams = [];
-    }
   }
 
 }
